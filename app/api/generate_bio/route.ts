a5c307a2e31dcf8b69203abaa667ac71b981ebbe
prudhvi <prudhvi@cloudflare.com>
2024-10-22 10:45:58 -0500

    updated prompt and model params

--- app/api/generate_bio/route.ts
+++ app/api/generate_bio/route.ts
@@ -9,21 +9,27 @@ export async function POST(request: NextRequest) {
     
     let { userInput, language, vibe } = await request.json() as { userInput: string, language: string, vibe: string }
   
-    const systemPrompt = `You are an expert LinkedIn bio writer. Your task is to create professional, engaging, and tailored LinkedIn bios based on the information provided. Always maintain a ${vibe} tone and write in ${language}. Focus on highlighting key skills, experiences, and unique qualities. Keep the bio concise, impactful, and optimized for LinkedIn's platform.`
+    const systemPrompt = `You are an expert LinkedIn bio writer. Your task is to create professional, engaging, and tailored LinkedIn bios based on the information provided. Focus on highlighting key skills, experiences, and unique qualities. Keep the bio concise, impactful, and optimized for LinkedIn's platform.`
 
-    const userPrompt = `Create a 50 words LinkedIn bio based on the following information: "${userInput}". The bio should be in ${language} and have a ${vibe} tone.`
+    const userPrompt = `Generate a ${vibe} LinkedIn bio in ${language} language. Make sure the generated biography is less than 300 characters, has short sentences that are found in LinkedIn about me sections, and use this information about the user as well: "${userInput}". Do not add any explanations or anything else, just one biography.`
 
     const messages = [
         { role: "system", content: systemPrompt },
         { role: "user", content: userPrompt },
     ]
 
-    const response = await ai.run("@cf/meta/llama-3.1-70b-instruct", { messages, temperature: 1 }, {
-        gateway: {
-          id: gateway_id,
-          skipCache: false,
-          cacheTtl: 3600000,
-        },
+    const response = await ai.run("@cf/meta/llama-3.1-70b-instruct",
+        { 
+            messages, 
+            temperature: 1, 
+            max_tokens: 512, 
+        }, 
+        {
+            gateway: {
+                id: gateway_id,
+                skipCache: false,
+                cacheTtl: 3600000,
+            },
       },
     )
 
