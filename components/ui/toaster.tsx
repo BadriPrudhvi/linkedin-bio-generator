99d18839db6c45cd715eb9997fb219c4a9daddbe
prudhvi <prudhvi@cloudflare.com>
2024-10-22 10:46:17 -0500

    added toast

---
+++ components/ui/toaster.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useToast } from "@/hooks/use-toast"
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
+import { Linkedin } from "lucide-react"
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && (
+                <ToastTitle className="flex items-center">
+                  <Linkedin className="w-4 h-4 mr-2 text-linkedin-blue" />
+                  {title}
+                </ToastTitle>
+              )}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
